7861e55556d50b91f07a4f308f2616b8f52d3b26
Piotr Stawirej <piotr.stawirej@gmail.com>
2015-09-22 00:08:19 +0200

    CountDiv - Refactor implementation.

--- src/main/java/codility/prefixsums/CountDiv.java
+++ src/main/java/codility/prefixsums/CountDiv.java
@@ -3,34 +3,15 @@ package codility.prefixsums;
 public class CountDiv {
 
     public int solution(final int A, final int B, final int K) {
+        final int newA = A - (A % K);
+        final int newB = B - (B % K);
 
-        final int newA = A % K == 0 ? A : A - (A % K);
-        final int newB = B % K == 0 ? B : B - (B % K);
+        final int divisibleCount = (newB - newA) / K;
 
-        if (K > B && B != 0) {
-            return 0;
-        } else if (A % K != 0 && B % K != 0 && A >= K) {
-            return (B - A) / K;
-        } else if (A % K == 0 && B % K != 0) {
-            return ((newB - A) / K) + 1;
-        } else if (A % K != 0 && B % K == 0) {
-            return (B - newA) / K;
-        } else if (A == 0 && B % K != 0) {
-            return newB / K;
-        } else if (A == 0 && B % K == 0) {
-            return (B / K) + 1;
-        } else if (A < K && A % K != 0) {
-            return (newB / K);
-        } else if (A == B) {
-            if (A % K == 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        } else if (A % K == 0 & B % K == 0) {
-            return ((B - A) / K) + 1;
+        if (A % K == 0) {
+            return divisibleCount + 1;
         } else {
-            return -1;
+            return divisibleCount;
         }
     }
 }
