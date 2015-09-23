d18eae1c0ccfc52502c695d7e258f35ce080d580
Piotr Stawirej <piotr.stawirej@gmail.com>
2015-09-23 15:04:05 +0200

    MinAvgTwoSlice - working implementation.

--- src/main/java/codility/prefixsums/MinAvgTwoSlice.java
+++ src/main/java/codility/prefixsums/MinAvgTwoSlice.java
@@ -4,45 +4,29 @@ public class MinAvgTwoSlice {
 
     public int solution(final int[] A) {
         float smallestAverage = Integer.MAX_VALUE;
+        float previousAverage = Integer.MAX_VALUE;
         int sliceStartIndex = 0;
-
         int startIndex = 0;
         int endIndex = 1;
-        float previousAvarage = Integer.MAX_VALUE;
 
-        for (; startIndex < A.length - 1;) {
-            final float currentAvarage = getAvarageForRange(A, startIndex, endIndex);
-            if (previousAvarage < currentAvarage) {
+        while (startIndex < A.length - 1) {
+            final float currentAverage = getAverageForRange(A, startIndex, endIndex);
+            if (currentAverage < previousAverage) {
+                if (smallestAverage > currentAverage) {
+                    smallestAverage = currentAverage;
+                    sliceStartIndex = startIndex;
+                }
+                endIndex = endIndex >= A.length - 1 ? A.length - 1 : endIndex + 1;
+            } else {
                 startIndex++;
                 endIndex = startIndex + 1;
-                continue;
             }
-            if (previousAvarage == currentAvarage) {
-                startIndex--;
-                if (startIndex < 0) {
-                    break;
-                } else {
-                    // startIndex++;
-                    // endIndex = endIndex >= A.length - 1 ? A.length - 1 : endIndex + 1;
-                }
-                previousAvarage = Integer.MAX_VALUE;
-            } else {
-                previousAvarage = currentAvarage;
-            }
-
-            if (smallestAverage > currentAvarage) {
-                smallestAverage = currentAvarage;
-                endIndex = endIndex >= A.length - 1 ? A.length - 1 : endIndex + 1;
-                sliceStartIndex = startIndex;
-            }
-
+            previousAverage = currentAverage;
         }
-
         return sliceStartIndex;
-
     }
 
-    private float getAvarageForRange(final int[] A, final int startIndex, final int endIndex) {
+    private float getAverageForRange(final int[] A, final int startIndex, final int endIndex) {
         int counter = 0;
         float sum = 0;
         for (int i = startIndex; i <= endIndex; i++) {
