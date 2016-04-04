427ef1a42688928c0ea5d054504ca8c61f36aafe
Piotr Stawirej <piotr.stawirej@gmail.com>
2016-04-04 21:53:19 +0200

    GenomicRangeQuery - add performance test.

--- src/test/java/codility/prefixsums/GenomicRangeQueryScenarios.java
+++ src/test/java/codility/prefixsums/GenomicRangeQueryScenarios.java
@@ -2,6 +2,8 @@ package codility.prefixsums;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class GenomicRangeQueryScenarios {
@@ -67,7 +69,7 @@ public class GenomicRangeQueryScenarios {
     }
 
     @Test
-    public void shouldPassMultipleSameLetter() {
+    public void shouldPassMultipleSameCharacter() {
         // Given
         final String S = "GGGGG"; // 33333
         final int[] P = {0, 0, 1, 1, 2, 3};
@@ -79,5 +81,30 @@ public class GenomicRangeQueryScenarios {
 
         // Then
         then(solution).containsSequence(3, 3, 3, 3, 3, 3).hasSize(6);
+    }
+
+    @Test(timeout = 7_000)
+    public void shouldPassMAximumLengthSameCharacter() {
+        // Given
+        final StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < 100_000; i++) {
+            buffer.append('G');
+        }
+        final String S = buffer.toString();
+
+        final int[] P = new int[50_000];
+        final int[] Q = new int[50_000];
+        Arrays.fill(P, 0);
+        Arrays.fill(Q, 99_000);
+
+        final GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
+
+        // When
+        final int[] solution = genomicRangeQuery.solution(S, P, Q);
+
+        // Then
+        final int[] expected = new int[50_000];
+        Arrays.fill(expected, 3);
+        then(solution).containsSequence(expected).hasSize(50_000);
     }
 }
