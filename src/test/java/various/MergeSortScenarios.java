580f976b6cec2fc510ea800c92b243734de88c4f
Piotr Stawirej <piotr.stawirej@gmail.com>
2021-11-12 21:55:47 +0100

    feat: merge sort case

--- src/test/java/various/MergeSortScenarios.java
+++ src/test/java/various/MergeSortScenarios.java
@@ -17,7 +17,8 @@ final class MergeSortScenarios {
             of(new int[] {1}, new int[] {1}),
             of(new int[] {2, 1}, new int[] {1, 2}),
             of(new int[] {3, 2, 1}, new int[] {1, 2, 3}),
-            of(new int[] {4, 3, 2, 1}, new int[] {1, 2, 3, 4})
+            of(new int[] {4, 3, 2, 1}, new int[] {1, 2, 3, 4}),
+            of(new int[] {5, 3, 8, 9, 1, 7, 0, 2, 6, 4}, new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
                      );
     }
 
